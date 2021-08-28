48918e84754d24882fa0a1f4fbc7391c8706988f
AmulyaGulati <amulyagulati@gmail.com>
2021-08-28 21:00:06 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6a1d45dc-0c17-4095-b7f6-bdb17659c87e';
+export const MODAL_CLOSE_SEC = 2.5;
